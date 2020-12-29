886f6a5cf40b1e873b38f847918305324b0bd6ce
yuanjinshuai01 <yuanjinshuai01@baidu.com>
2020-12-29 19:50:01 +0800

    fix cros

--- covid_19_case.js
+++ covid_19_case.js
@@ -3,6 +3,7 @@ async function getData(){
     const api_url = 'https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.json';
     const response = await fetch(api_url);
     const data = await response.json();
+    const {}
     console.log(data);
 }
 
--- sketch.js
+++ sketch.js
@@ -1,40 +1,19 @@
 async function getData(){
-    // const api_url = "https://api.wheretheiss.at/v1/satellites/25544"
-    // const api_url = "https://yjspersonal-website-back.herokuapp.com/test";
-    // fetch(api_url);
-    // const response = await fetch(api_url);
-    // const data = await response.json();
+    const api_url = "https://yjspersonal-website-back.herokuapp.com/test";
+    // {
+    //     "aa":"bb",
+    //     "foo":"hello json",
+    //     "key":"value"
+    // }
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log( data);
 
-    const url = "https://yjspersonal-website-back.herokuapp.com/test"; // site that doesn’t send Access-Control-*
-    fetch(url)
-    .then(response => response.text())
-    .then(contents => console.log(contents))
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-
-
-    
-    // console.log(request)
-    // console.log(data);`
-    // const {latitude, longitude} = data;
-    // document.getElementById('lat').textContent = latitude;
-    // document.getElementById('lon').textContent = longitude;
-    
-    // document.getElementById('res').textContent = data;
-
-    // var request = new Request('http://180.76.147.17:8080/test');
-    // const response  = await fetch(request, {mode: 'no-cors'});
-    // const data = response;
-    // console.log(data);
-    // console.log(request);
-    // const response = await fetch(request);
-    // console.log("now print response")
-    // console.log(response);
-
-    // const data = await response;
-    // console.log("data" + data);
-    // console.log(data);
-    // document.getElementById('res').textContent = data;
-    
+    const {aa, foo} = data;
+    console.log(aa);
+    console.log(foo);
+    document.getElementById('a').textContent = aa;
+    document.getElementById('b').textContent = foo;
+    console.log(data);
 }
-
 getData();
