ca9bc22fdcd1d04a101cde11c2ef600291db83dd
yuanjinshuai01 <yuanjinshuai01@baidu.com>
2020-12-29 10:10:47 +0800

    add tool

---
+++ covid_19_case.js
@@ -0,0 +1,9 @@
+async function getData(){
+    console.log("start to run");
+    const api_url = 'https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.json';
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data);
+}
+
+getData();
--- sketch.js
+++ sketch.js
@@ -1,23 +1,30 @@
 async function getData(){
     // const api_url = "https://api.wheretheiss.at/v1/satellites/25544"
-    // const api_url = "http://180.76.147.17:8080/test"
+    // const api_url = "https://yjspersonal-website-back.herokuapp.com/test";
+    // fetch(api_url);
     // const response = await fetch(api_url);
     // const data = await response.json();
+
+    const url = "https://yjspersonal-website-back.herokuapp.com/test"; // site that doesn’t send Access-Control-*
+    fetch(url)
+    .then(response => response.text())
+    .then(contents => console.log(contents))
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 
 
     
     // console.log(request)
-    // console.log(data);
+    // console.log(data);`
     // const {latitude, longitude} = data;
     // document.getElementById('lat').textContent = latitude;
     // document.getElementById('lon').textContent = longitude;
     
     // document.getElementById('res').textContent = data;
 
-    var request = new Request('http://180.76.147.17:8080/test');
-    const response  = await fetch(request, {mode: 'no-cors'});
-    const data = response;
-    console.log(data);  
+    // var request = new Request('http://180.76.147.17:8080/test');
+    // const response  = await fetch(request, {mode: 'no-cors'});
+    // const data = response;
+    // console.log(data);
     // console.log(request);
     // const response = await fetch(request);
     // console.log("now print response")
